80e3752411dbb6c3b9070f3a1f5415367c8e91fa
Calum Hamilton <calum.hamilton@outlook.com>
2024-09-14 11:55:24 +0100

    Working attaching of objects - single and multiple objects

--- Assets/Scripts/DragDropItemScript.cs
+++ Assets/Scripts/DragDropItemScript.cs
@@ -9,7 +9,6 @@ public class DragDropItemScript : MonoBehaviour
     // The difference between where the mouse is on the drag plane and 
     // where the origin of the object is on the drag plane
     private Vector3 offset;
-
     private Camera myMainCamera; 
 
     void Start()
@@ -17,22 +16,55 @@ public class DragDropItemScript : MonoBehaviour
         myMainCamera = Camera.main; // Camera.main is expensive - cache it here
     }
 
+    private void Update()
+    {
+        // Fixing issue where multiple joined items are rotating
+        transform.SetPositionAndRotation(transform.position, new Quaternion(transform.rotation.x, transform.rotation.y, 0, transform.rotation.w));
+    }
+
     void OnMouseDown()
     {
         dragPlane = new Plane(myMainCamera.transform.forward, transform.position); 
-        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition); 
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition);
 
-        float planeDist;
-        dragPlane.Raycast(camRay, out planeDist); 
+        dragPlane.Raycast(camRay, out float planeDist);
         offset = transform.position - camRay.GetPoint(planeDist);
+
+        // Destroy joint if it exists
+        if (gameObject.TryGetComponent(out FixedJoint specificJoint))
+        {
+            // Destroy the specific joint
+            Destroy(specificJoint);
+        }
     }
 
     void OnMouseDrag()
     {   
-        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition); 
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition);
 
-        float planeDist;
-        dragPlane.Raycast(camRay, out planeDist);
+        dragPlane.Raycast(camRay, out float planeDist);
         transform.position = camRay.GetPoint(planeDist) + offset;
+    }
+
+    void OnMouseUp()
+    {
+        // Check if the object being dragged is touching any other objects
+        Collider[] colliders = Physics.OverlapSphere(gameObject.transform.position, 1f);
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.gameObject != gameObject)
+            {
+                // If touching another object, create a joint between them
+                FixedJoint joint = gameObject.AddComponent<FixedJoint>();
+                joint.connectedBody = collider.attachedRigidbody;
+
+                // Optionally set joint parameters like break force, break torque, etc.
+                joint.breakForce = 500f;
+                joint.breakTorque = 500f;
+
+                break; // We only need one joint, break out of the loop
+            }
+        }
     }
 }
